cc013d9f11e5003ee8c042594e03cc22de8188ed
AnastasiiaV0304 <volosina608@gmail.com>
2024-01-14 12:03:56 +0200

    add changes

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+const boxesContainer = document.getElementById("boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement("div");
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      box.classList.add("box");
+      fragment.appendChild(box);
+    }
+
+    boxesContainer.appendChild(fragment);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
